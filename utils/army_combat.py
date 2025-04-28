3f3acad2dc2dad8f2e6cdfd66ef989035c02ea99
yanny526 <yannytaongamareva@gmail.com>
2025-04-28 15:30:09 +0200

    Update army_combat.py

--- utils/army_combat.py
+++ utils/army_combat.py
@@ -1,1 +1,13 @@
+# army_combat.py
+
+def calculate_battle_outcome(player_army, target_army):
+    player_power = sum([UNIT_STATS.get(unit, {}).get("attack", 0) * count for unit, count in player_army.items()])
+    target_power = sum([UNIT_STATS.get(unit, {}).get("attack", 0) * count for unit, count in target_army.items()])
+
+    if player_power > target_power:
+        return "Victory"
+    elif player_power < target_power:
+        return "Defeat"
+    else:
+        return "Draw"
 
