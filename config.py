ab23300cfd6024d0ec24827de32ae3663ccec99f
yanny526 <yannytaongamareva@gmail.com>
2025-05-02 03:44:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-import os
-from base64 import b64decode
-import json
+# config.py
 
+import os
+import json
+from base64 import b64decode
+
+# Environment variables
 BASE64_CREDS = os.getenv('BASE64_CREDS')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 SHEET_ID = os.getenv('SHEET_ID')
 
-# Decode service account
+# Decode the base64-encoded service account JSON
 SERVICE_ACCOUNT_INFO = json.loads(b64decode(BASE64_CREDS).decode())
