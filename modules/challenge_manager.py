81b2a8ac39549d30806406e678d9482979e6a658
yanny526 <yannytaongamareva@gmail.com>
2025-05-03 23:58:18 +0200

    Update challenge_manager.py

--- modules/challenge_manager.py
+++ modules/challenge_manager.py
@@ -58,7 +58,6 @@ def update_player_progress(uid: str, ch: Challenge, increment: int = 1):
         if prog >= ch.value and not prow[3]:
             prow[3] = today
         update_row("PlayerChallenges", pc_idx, prow)
-
     else:
         # new entry: [player_id,challenge_id,date_assigned,date_completed,progress_count,awarded]
         completed = today if increment >= ch.value else ""
@@ -76,8 +75,15 @@ def award_challenges(uid: str, ch_type: str):
     challenges = load_challenges(ch_type)
     for ch in challenges:
         pc_idx, prow = get_player_challenge(uid, ch.id)
+        if not prow:
+            continue
+
+        # Ensure prow has at least 6 elements: [player_id, cid, assigned, completed, progress_count, awarded]
+        while len(prow) < 6:
+            prow.append('')
+
         # prow[3] = date_completed, prow[5] = awarded flag
-        if prow and prow[3] and prow[5] != 'awarded':
+        if prow[3] and prow[5] != 'awarded':
             # grant resources
             players = get_rows("Players")
             header, pdata = players[0], players[1:]
@@ -87,6 +93,7 @@ def award_challenges(uid: str, ch_type: str):
             row[4] = str(int(row[4]) + ch.reward_minerals)
             row[5] = str(int(row[5]) + ch.reward_energy)
             update_row("Players", pidx, row)
-            # mark awarded
+
+            # mark as awarded
             prow[5] = 'awarded'
             update_row("PlayerChallenges", pc_idx, prow)
