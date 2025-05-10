771ce2a4c6687f4abfe87a34c3cb8985279415f0
yanny526 <yannytaongamareva@gmail.com>
2025-05-10 19:49:44 +0200

    Update discovery.py

--- googleapiclient/discovery.py
+++ googleapiclient/discovery.py
@@ -1,1 +1,33 @@
-def build(*args, **kwargs): class S: pass; return S()
+# googleapiclient/discovery.py
+
+def build(*args, **kwargs):
+    """
+    Stub for googleapiclient.discovery.build
+    Returns a minimal service object with spreadsheets().values().get/append/update stub methods.
+    """
+    class DummyService:
+        def spreadsheets(self):
+            return self
+
+        def values(self):
+            return self
+
+        def get(self, *args, **kwargs):
+            class Exec:
+                def execute(self):
+                    return {"values": []}
+            return Exec()
+
+        def append(self, *args, **kwargs):
+            class Exec:
+                def execute(self):
+                    return {}
+            return Exec()
+
+        def update(self, *args, **kwargs):
+            class Exec:
+                def execute(self):
+                    return {}
+            return Exec()
+
+    return DummyService()
