778d3ce2dd3701ce95e35b6e33a02faedacd6e11
yanny526 <yannytaongamareva@gmail.com>
2025-05-19 19:16:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-
 import os
-import asyncio
 from telegram.ext import Application
 from core_handlers import get_conversation_handler
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-async def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(get_conversation_handler())
-    print("✅ SkyHustle bot is live...")
-    await application.run_polling()
+application = Application.builder().token(BOT_TOKEN).build()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# Register all handlers
+application.add_handler(get_conversation_handler())
+
+print("✅ SkyHustle bot is live...")
+application.run_polling()
